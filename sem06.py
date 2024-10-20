bf6378ca63bbf2d3164d04351a338c1769c122ca
matthew.devian <98856728+JHONY44@users.noreply.github.com>
2024-10-19 20:52:26 -0500

    4

--- sem06.py
+++ sem06.py
@@ -21,4 +21,19 @@ num=st.number_input("ingrese un numero para ver su tabla de multiplicar del 1 al
 if st.button("ejecitar ejercicio 3"):
     for i in range (1,13):
         st.write(f"{num} x {i} ={num * i}")
-        
+
+#ejercicio 4: calcular la media y comparar con 10
+st.subheader("ejercicio 4: comparar 10 numeros con el valor 10")
+numeros_ej1= st.text_input("ingresa 10 numeros separados por comas: ")
+if st.button("ejecutar ejercicio 4"):
+    lista_numeros= [int(num) for num in numeros_ej1.split(",")]
+    media=sum(lista_numeros)/len(lista_numeros)
+    mayores=len([num for num in lista_numeros if num>10])
+    iguales= len([num for num in lista_numeros if num ==10])
+    menores= len([num for num in lista_numeros if num<10])
+
+    st.write(f"la media es: {media}")
+    st.write(f"mayores que 10: {mayores}")
+    st.write(f"iguales a 10: {iguales}")
+    st.write(f"menores que 10: {menores}")
+    
