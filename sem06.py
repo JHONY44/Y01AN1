296bfc3601223a3216fea3e608c7cdc377e7d67c
matthew.devian <98856728+JHONY44@users.noreply.github.com>
2024-10-19 20:24:01 -0500

    s

--- sem06.py
+++ sem06.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 #titulo de la aplicacion
-st.tittle("ejercicios con bucles basicos en python")
+st.title("ejercicios con bucles basicos en python")
 
 #ejercicio 1 :imprimir 10 veces "hola mundo"
 st.subheader("ejercicio 1: imprimir 'hola mundo' diez veces ")
 if st.button("ejecutar ejercicio 1"):
     for i in range (10):
         st.write("hola mundo")
-        
